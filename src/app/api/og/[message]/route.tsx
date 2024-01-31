56ac68289a5444fa23780f35c4ea951a21ec0156
Julien Genestoux <julien.genestoux@gmail.com>
2024-01-30 19:27:47 -0500

    single route for no-wallet

--- src/app/api/og/[message]/route.tsx
+++ src/app/api/og/[message]/route.tsx
@@ -7,15 +7,19 @@ export async function GET(
   { params }: { params: { message: string } }
 ) {
   const u = new URL(request.url);
-  const message = await getMessage(params.message);
 
-  let content = message.description;
-  if (u.searchParams.get("state") === "clear") {
-    content = message.body;
-  } else if (u.searchParams.get("state") === "hidden") {
-    content = "Sorry, you need to get a membership!";
+  let content = "";
+  if (params.message === "no-wallet") {
+    content = "Please link your farcaster account to a wallet!";
+  } else {
+    const message = await getMessage(params.message);
+    content = message.description;
+    if (u.searchParams.get("state") === "clear") {
+      content = message.body;
+    } else if (u.searchParams.get("state") === "hidden") {
+      content = "Sorry, you need to get a membership!";
+    }
   }
-
   return new ImageResponse(<Message content={content} />, {
     width: 1200,
     height: 630,
