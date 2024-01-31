807e27a8011161722216327d160efa8f94c75315
Julien Genestoux <julien.genestoux@gmail.com>
2024-01-31 11:08:12 -0500

    updated style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,48 @@ const inter = Inter({ subsets: ["latin"] });
 export default async function Home() {
   return (
     <main className="p-8">
-      <h1>Token Gated Frames</h1>
-      <p>
-        <Link href="https://warpcast.notion.site/Farcaster-Frames-4bd47fe97dc74a42a48d3a234636d8c5">
+      <h1 className="text-xl font-bold">Token Gated Frames</h1>
+      <p className="mt-2">
+        <Link
+          target="_blank"
+          className="underline"
+          href="https://warpcast.notion.site/Farcaster-Frames-4bd47fe97dc74a42a48d3a234636d8c5"
+        >
           Frames
         </Link>{" "}
         are an interation on top of{" "}
-        <Link href="https://ogp.me/">OpenGraph</Link>.
+        <Link target="_blank" className="underline" href="https://ogp.me/">
+          OpenGraph
+        </Link>
+        .
       </p>
-      <p>
-        At <Link href="https://unlock-protocol.com/">Unlock</Link> we built a
-        protocol for membership. You can now <em>token-gate your frames</em> so
-        that only active members can see their content!
+      <p className="mt-2">
+        At{" "}
+        <Link
+          target="_blank"
+          className="underline"
+          href="https://unlock-protocol.com/"
+        >
+          Unlock
+        </Link>{" "}
+        we built a protocol for membership. You can now{" "}
+        <em>token-gate your frames</em> so that only active members can see
+        their content!
       </p>
-      <p>
-        <Link href="https://warpcast.com/julien51.eth/0xa5ec13e9c1bc2145d3e378b05886c36d5711de37">
+      <p className="mt-2">
+        <Link
+          target="_blank"
+          className="underline"
+          href="https://warpcast.com/julien51.eth/0xa5ec13e9c1bc2145d3e378b05886c36d5711de37"
+        >
           Demo
         </Link>{" "}
         |{" "}
-        <Link href="https://github.com/unlock-protocol/token-gated-frame">
+        <Link
+          target="_blank"
+          className="underline"
+          href="https://github.com/unlock-protocol/token-gated-frame"
+        >
           Source code
         </Link>
       </p>
