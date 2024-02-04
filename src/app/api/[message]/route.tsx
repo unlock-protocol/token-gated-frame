f23adc426c68e133a817daf28cb0b62325fbc6e3
Julien Genestoux <julien.genestoux@gmail.com>
2024-02-03 20:40:43 -0500

    Revert "removed debugging tool"
    This reverts commit c11f995cbe6b48519ac3d30bd36385c7ffacdd31.

--- src/app/api/[message]/route.tsx
+++ src/app/api/[message]/route.tsx
@@ -22,3 +22,13 @@ export async function POST(
     fcMessage.message.data.fid
   );
 }
+
+// REMOVE ME FOR PROD!
+export async function GET(
+  request: Request,
+  { params }: { params: { message: string } }
+) {
+  const u = new URL(request.url);
+  const fid = u.searchParams.get("fid");
+  return renderMessageForFid(u.origin, params.message, fid);
+}
